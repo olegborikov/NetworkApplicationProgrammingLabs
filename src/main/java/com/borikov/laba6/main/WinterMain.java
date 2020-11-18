b2b7aa80d6646f5412b06e4916e6ab404784e49b
Oleg Borikov <olegborikov999@gmail.com>
2020-11-18 23:29:21 +0300

    complete laboratory work 6(dont realize stop button)

---
+++ src/main/java/com/borikov/laba6/main/WinterMain.java
@@ -0,0 +1,10 @@
+package com.borikov.laba6.main;
+
+import com.borikov.laba6.entity.WinterThread;
+
+public class WinterMain {
+    public static void main(String[] args) {
+        WinterThread winterThread = new WinterThread();
+        winterThread.setVisible(true);
+    }
+}
